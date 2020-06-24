413f51da9724291efd373afdc25b1c4411aa8d2c
宋佳恒 <jiaheng.song@me2zen.com>
2020-06-24 14:47:05 +0800

    删除多余内容

--- build.bat
+++
@@ -1,3 +0,0 @@
-rem please add chrome.exe to PATH
-chrome.exe --pack-extension=%cd%/bin/ --pack-extension-key=%cd%/cert/CocosDevTools.pem
-move bin.crx %cd%/bin/CocosDevTools.crx
--- deploy/start.bat
+++
@@ -1,2 +0,0 @@
-scp -i cocos-devtools-key.pem ../bin_web/cocos-devtools-web.min.js root@211.152.2.89:/home/cocos-devtools/static/js/
-scp -i cocos-devtools-key.pem ../gh-pages/* root@211.152.2.89:/home/cocos-devtools/static/cocos-devtools/
